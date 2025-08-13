549b44872b4f7b38b38cdb5dec1e913fc19e656c
anas <kilanyanas2@gmail.com>
2025-08-13 16:03:52 +0300

    fix users page

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const { isAuthenticated, isEditor } = require('../middleware/auth');
+const { isAuthenticated, isAdmin } = require('../middleware/auth');
 
 // عرض قائمة المستخدمين
-router.get('/', isAuthenticated, isEditor, userController.getUsers);
+router.get('/', isAuthenticated, isAdmin, userController.getUsers);
 
 // عرض نموذج إضافة مستخدم جديد
-router.get('/create', isAuthenticated, isEditor, userController.getCreateForm);
+router.get('/create', isAuthenticated, isAdmin, userController.getCreateForm);
 
 // إضافة مستخدم جديد
-router.post('/create', isAuthenticated, isEditor, userController.createUser);
+router.post('/create', isAuthenticated, isAdmin, userController.createUser);
 
 // عرض نموذج تعديل مستخدم
-router.get('/:id/edit', isAuthenticated, isEditor, userController.getEditForm);
+router.get('/:id/edit', isAuthenticated, isAdmin, userController.getEditForm);
 
 // تحديث بيانات المستخدم
-router.post('/:id/edit', isAuthenticated, isEditor, userController.updateUser);
+router.post('/:id/edit', isAuthenticated, isAdmin, userController.updateUser);
 
 // حذف مستخدم
-router.post('/delete/:id', isAuthenticated, isEditor, userController.deleteUser);
+router.post('/delete/:id', isAuthenticated, isAdmin, userController.deleteUser);
 
 module.exports = router; 
